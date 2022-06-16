2a916afbdaef749a6f7e53d3311ade8aaad096e9
React <3809164@gmail.com>
2022-06-16 22:44:35 +0300

    lesson14

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import products from "./Products.module.css"
+import ProductsCars from "./ProductsCars/ProductsCars";
 
 const Products = () =>{
+    let titlePage = "Products"
+    const yaerCar = Math.round(Math.random() *10000)
     return(
         <div className={products.products}>
-            <h2 className={products.title}>products</h2>
-            <p>{Math.round(Math.random() *100)}</p>
+            <ProductsCars name={"bmw"} year={yaerCar} titlePage={titlePage}/>
+            <ProductsCars name={"mazda"} year={yaerCar}/>
         </div>
     )
 }
